2037b4c9fa6adde024dae3ddd73b689c33fdfa07
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-07-01 20:33:58 +0600

    add Pricing

--- components/Sections/Pricing/Pricing.tsx
+++ components/Sections/Pricing/Pricing.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 export const Pricing = () => {
     return (
-        <div>
-            Pricing
+        <div className='my-6 md:my-16'>
+            <h2 className='text-xl font-semibold mb-8 uppercase'>Our Rates</h2>
+            <div className='flex flex-col md:flex-row justify-between gap-4'>
+                <div className='md:w-1/4 p-3 rounded-lg border border-gray-300 bg-white'>
+                    <h3 className='text-lg font-light mb-3'>Standard Rate</h3>
+                    <h4 className='text-4xl font-light'>$25/hr</h4>
+                </div>
+                <div className='md:w-1/4 p-3 rounded-lg border border-gray-300 bg-white'>
+                    <h3 className='text-lg font-light mb-3'>Mid-Length Contract</h3>
+                    <h4 className='text-4xl mb-4 font-light'>$22/hr</h4>
+                    <p>contracts ranging from <b>one week</b> to one month</p>
+                </div>
+                <div className='md:w-2/4 px-6 py-3 rounded-lg border border-gray-300 bg-gradient text-white'>
+                    <h3 className='text-lg font-light mb-3'>Long-Term Contract</h3>
+                    <h4 className='text-4xl mb-4 font-light'>$17/hr</h4>
+                    <p>contracts for <b>one month</b> and longer</p>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: theme => ({
+                'gradient': 'linear-gradient(140deg, rgba(140,117,252,1) 0%, rgba(105,161,253,1) 55%, rgba(235,132,252,1) 100%);',
+            })
+        },
+    },
+    plugins: [],
 }
 
